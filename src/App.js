00eaecf3370294f208343c47508853c373078396
JayEee2U <j_double_u@icloud.com>
2025-01-12 19:11:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.scss";
+
+function Header() {
+  return <header></header>;
+}
+
+export default Header;
